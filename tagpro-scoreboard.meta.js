c07c06ba4ec0a9c9846465f14aca686ed65e9c44
Kera <keratagpro@gmail.com>
2015-01-20 19:25:47 +0200

    Updates

---
+++ tagpro-scoreboard.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name          TagPro Scoreboard
+// @description   Player/Team/Powerup/Score overview for TagPro, mainly for streamers.
+// @version       0.3
+// @author        Kera
+// @grant         GM_getValue
+// @grant         GM_setValue
+// @namespace     http://keratagpro.github.io/tagpro-scoreboard
+// @icon          http://keratagpro.github.io/tagpro-scoreboard/images/score.png
+// @downloadUrl   https://keratagpro.github.io/tagpro-scoreboard/tagpro-scoreboard.user.js
+// @updateUrl     https://keratagpro.github.io/tagpro-scoreboard/tagpro-scoreboard.meta.js
+// @include       http://tagpro-*.koalabeast.com:*
+// @include       http://tangent.jukejuice.com:*
+// @include       http://*.newcompte.fr:*
+// ==/UserScript==
